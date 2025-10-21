97d4148b6e1304b3593996d322cb9e8497702606
Burak Arslan <burak.arslan@arskom.com.tr>
2025-10-21 21:27:13 +0300

    Build output goes to ../assets
    Change-Id: I6f61475b14600ccece41c7da474ea6ba0b9f2d09

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -4,4 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+    build: {
+    outDir: '../assets',
+    emptyOutDir: true, // also necessary
+  }
+
 })
